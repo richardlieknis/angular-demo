49b70b3310870ccc1f0b8bff7820da35b55476dc
Richard Lieknis <richard.lieknis@web.de>
2023-03-21 20:11:59 +0100

    slideshow updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ersteApp';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'notebook.jpg', 'tum.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(()=> {
+        this.showImage = true;
+      }, 1)
+    }, 5000)
+  }
 }
