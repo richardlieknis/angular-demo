fffa37ea5f08e95ed0017a708aac08c5de6c990a
Richard Lieknis <richard.lieknis@web.de>
2023-03-22 10:23:30 +0100

    darkmode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = "TEST";
+  constructor(public router: Router){}
 }
